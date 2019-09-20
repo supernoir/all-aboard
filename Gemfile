e19adbc1ccaec7675a40142257f16deaa4460e9a
supernoir <kawohl.bentodesign@gmail.com>
2019-09-21 00:59:03 +0200

    adds dockerfile, sysinfo

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org' do
+  gem 'sinatra', '~> 2.0', '>= 2.0.7'
+  gem 'sysinfo', '0.8.1'
+end
--- app.rb
+++ app.rb
@@ -1,13 +1,21 @@
 require 'sinatra'
+# require 'sinatra/base'
 require 'erb'
+require 'sysinfo'
 
 set :public_folder, '/views/public'
 set :port, 8080
 
-
-get '/' do
-  @useragent = request.user_agent
-  @host = request.host
-  erb :whoami
+# Defines AllAboardApp as Sinatra base
+class AllAboardApp < Sinatra::Base
+  get '/' do
+    sysinfo = SysInfo.new
+    @useragent = request.user_agent
+    @host = request.host
+    @vm = sysinfo.vm
+    @impl = sysinfo.impl
+    @arch = sysinfo.arch
+    @platform = sysinfo.platform
+    erb :whoami
+  end
 end
-
